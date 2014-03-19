a731a6f222ff6a123c9a53445f9ac4fc3750b438
Vardan Akopian <vakopian@sustain.net>
2014-03-19 15:50:49 -0700

    initial import

---
+++ src/main/java/org/apache/jasper/runtime/ChunkedBuffer.java
@@ -0,0 +1,473 @@
+package org.apache.jasper.runtime;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.io.Writer;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: vakopian
+ * Date: 3/17/14
+ * Time: 3:49 PM
+ */
+public class ChunkedBuffer {
+    public static final int DEFAULT_INITIAL_CAPACITY = 512;
+    public static final double DEFAULT_GROWTH_FACTOR = 1.5;
+    public static final int MIN_CHUNK_SIZE = 16;
+    public static final int DEFAULT_MAX_CHUNK_SIZE = 16 * 1024;
+
+    private final int initialCapacity;
+    private final double growthFactor;
+    private final int maxChunkSize;
+
+    private List<char[]> chunks;
+
+    private int currentChunkIdx = -1;
+    private char[] currentChunk = null;
+    private int posInCurrentChunk;
+
+    private int count;
+    private int capacity;
+
+    public ChunkedBuffer(String text) {
+        this(DEFAULT_INITIAL_CAPACITY);
+        append(text);
+    }
+
+    public ChunkedBuffer() {
+        this(DEFAULT_INITIAL_CAPACITY);
+    }
+
+    public ChunkedBuffer(int initialCapacity) {
+        this(initialCapacity, DEFAULT_MAX_CHUNK_SIZE);
+    }
+
+
+    public ChunkedBuffer(int initialCapacity, int maxChunkSize) {
+        this(initialCapacity, maxChunkSize, DEFAULT_GROWTH_FACTOR);
+    }
+
+    public ChunkedBuffer(int initialCapacity, int maxChunkSize, double growthFactor) {
+        if (initialCapacity <= 0) {
+            throw new IllegalArgumentException("initialCapacity must be positive");
+        }
+        if (growthFactor < 1) {
+            throw new IllegalArgumentException("growthFactor must be at least 1");
+        }
+        if (maxChunkSize < initialCapacity) {
+            throw new IllegalArgumentException("maxChunkSize must be at least as large as initialCapacity");
+        }
+        this.initialCapacity = initialCapacity;
+        this.growthFactor = growthFactor;
+        this.maxChunkSize = maxChunkSize;
+
+        this.chunks = new ArrayList<char[]>();
+        this.currentChunk = new char[initialCapacity];
+        this.chunks.add(this.currentChunk);
+        this.currentChunkIdx = 0;
+        this.posInCurrentChunk = 0;
+        this.count = 0;
+        this.capacity = initialCapacity;
+    }
+
+    public void append(char character) {
+        ensureCapacityInternal(count + 1);
+        ensureCurrentChunkHasCapacity();
+        currentChunk[posInCurrentChunk++] = character;
+        this.count++;
+    }
+
+    public void append(String text) {
+        append(text, 0, text.length());
+    }
+
+    public void append(String text, int start, int length) throws IllegalArgumentException {
+        if (length <= 0) {
+            return;
+        }
+        if (text == null) {
+            throw new IllegalArgumentException("characters: may not be null.");
+        }
+        if (start < 0) {
+            throw new IllegalArgumentException("start: points beyond end of array.");
+        }
+        if (length < 0) {
+            throw new IllegalArgumentException("length: specifies length in excess of array length.");
+        }
+        if (start > text.length() || length > text.length() || start + length < 0 || (start + length) > text.length()) {
+            throw new StringIndexOutOfBoundsException("length: specifies length in excess of array length.");
+        }
+
+        ensureCapacityInternal(count + length);
+        while (length > 0) {
+            int charsToCopy = ensureCurrentChunkHasCapacity();
+            if (charsToCopy > length) {
+                charsToCopy = length;
+            }
+            if (charsToCopy > 0) {
+                text.getChars(start, start + charsToCopy, currentChunk, posInCurrentChunk);
+                start += charsToCopy;
+                length -= charsToCopy;
+                this.posInCurrentChunk += charsToCopy;
+                this.count += charsToCopy;
+            }
+        }
+    }
+
+    private int ensureCurrentChunkHasCapacity() {
+        int currentChunkCapacity = currentChunk.length - posInCurrentChunk;
+        if (currentChunkCapacity > 0) {
+            return currentChunkCapacity;
+        }
+        // go to the next chunk
+        assert currentChunkIdx + 1 < chunks.size();
+        posInCurrentChunk = 0;
+        currentChunkIdx++;
+        currentChunk = chunks.get(currentChunkIdx);
+        return currentChunk.length;
+    }
+
+    public void append(char[] characters, int start, int length) throws IllegalArgumentException {
+        if (length <= 0) {
+            return;
+        }
+        if (characters == null) {
+            throw new IllegalArgumentException("characters: may not be null.");
+        }
+        if (start < 0) {
+            throw new IllegalArgumentException("start: points beyond end of array.");
+        }
+        if (length < 0) {
+            throw new IllegalArgumentException("length: specifies length in excess of array length.");
+        }
+        if (start > characters.length || length > characters.length || start + length < 0 || (start + length) > characters.length) {
+            throw new StringIndexOutOfBoundsException("length: specifies length in excess of array length.");
+        }
+
+        ensureCapacityInternal(count + length);
+        while (length > 0) {
+            int charsToCopy = ensureCurrentChunkHasCapacity();
+            if (charsToCopy > length) {
+                charsToCopy = length;
+            }
+
+            if (charsToCopy > 0) {
+                System.arraycopy(characters, start, currentChunk, posInCurrentChunk, charsToCopy);
+                start += charsToCopy;
+                length -= charsToCopy;
+                this.posInCurrentChunk += charsToCopy;
+                this.count += charsToCopy;
+            }
+        }
+    }
+
+    public void getChars(int srcBegin, int srcEnd, char[] dst, int dstBegin)
+    {
+        if (srcBegin < 0)
+            throw new StringIndexOutOfBoundsException(srcBegin);
+        if (dstBegin < 0)
+            throw new StringIndexOutOfBoundsException(dstBegin);
+        if (dstBegin >= dst.length)
+            throw new StringIndexOutOfBoundsException(dstBegin);
+        if ((srcEnd < 0) || (srcEnd > count))
+            throw new StringIndexOutOfBoundsException(srcEnd);
+        if (srcBegin > srcEnd)
+            throw new StringIndexOutOfBoundsException("srcBegin > srcEnd");
+        if (srcBegin > count)
+            throw new StringIndexOutOfBoundsException("srcBegin > length()");
+        int length = srcEnd - srcBegin;
+        if (length > count)
+            throw new StringIndexOutOfBoundsException("srcEnd - srcBegin > length()");
+        if (dstBegin + length > count)
+            throw new StringIndexOutOfBoundsException("dstBegin + srcEnd - srcBegin > length()");
+        if (length <= 0) {
+            return;
+        }
+
+        int i = 0;
+        int iEnd = chunks.size();
+        for (; i < iEnd; i++) {
+            char[] chunk = chunks.get(i);
+            if (chunk.length >= srcBegin) {
+                break;
+            }
+            srcBegin -= chunk.length;
+        }
+        int offset = dstBegin;
+        int srcStart = srcBegin;
+        for (; i < iEnd; i++) {
+            char[] chunk = this.chunks.get(i);
+            int charsToCopy = chunk.length - srcStart;
+            if (charsToCopy > length) {
+                charsToCopy = length;
+            }
+            System.arraycopy(chunk, srcStart, dst, offset, charsToCopy);
+            offset += charsToCopy;
+            length -= charsToCopy;
+            srcStart = 0;
+            if (length == 0) {
+                break;
+            }
+        }
+    }
+
+    public char[] toArray() {
+        char[] result = new char[length()];
+        int offset = 0;
+        for (char[] chunk : this.chunks) {
+            if (chunk == currentChunk) {
+                break;
+            }
+            System.arraycopy(chunk, 0, result, offset, chunk.length);
+            offset += chunk.length;
+        }
+        System.arraycopy(this.currentChunk, 0, result, offset, posInCurrentChunk);
+        return result;
+    }
+
+    public String toString() {
+        StringBuilder sb = new StringBuilder(length());
+        for (char[] chunk : this.chunks) {
+            if (chunk == currentChunk) {
+                break;
+            }
+            sb.append(chunk);
+        }
+        sb.append(currentChunk, 0, posInCurrentChunk);
+        return sb.toString();
+    }
+
+    public int capacity() {
+        return capacity;
+    }
+
+    public int length() {
+        return this.count;
+    }
+
+    public void clear() {
+        chunks.clear();
+        currentChunk = null;
+        currentChunkIdx = -1;
+        capacity = 0;
+        count = 0;
+    }
+
+    public void setLength(int newLength) {
+        if (newLength < 0)
+            throw new IllegalArgumentException();
+        ensureCapacityInternal(newLength);
+        this.count = newLength;
+        for (int i = 0, chunks1Size = this.chunks.size(); i < chunks1Size; i++) {
+            char[] chunk = this.chunks.get(i);
+            this.currentChunk = chunk;
+            this.posInCurrentChunk = newLength;
+            this.currentChunkIdx = i;
+            if (chunk.length >= newLength) {
+                break;
+            }
+            newLength -= chunk.length;
+        }
+    }
+
+    public void trimToSize() {
+        chunks.subList(currentChunkIdx + 1, chunks.size()).clear();
+    }
+
+    private void ensureCapacityInternal(int newCapacity) {
+        if (newCapacity < 0)
+            throw new IllegalArgumentException();
+        while (newCapacity >= this.capacity) {
+            // allocate a new chunk
+            int size = capacity == 0 ? this.initialCapacity : (int) (capacity * growthFactor) - capacity;
+            if (size < MIN_CHUNK_SIZE) {
+                size = MIN_CHUNK_SIZE;
+            }
+            if (size > maxChunkSize) {
+                size = maxChunkSize;
+            }
+            this.chunks.add(new char[size]);
+            this.capacity += size;
+        }
+        if (currentChunk == null && chunks.size() > 0) {
+            currentChunkIdx = 0;
+            currentChunk = chunks.get(0);
+            posInCurrentChunk = 0;
+            count = 0;
+        }
+    }
+
+    public int getUnused() {
+        int sz = currentChunk.length - posInCurrentChunk;
+        for (int i = currentChunkIdx + 1, chunksSize = chunks.size(); i < chunksSize; i++) {
+            sz += chunks.get(i).length;
+        }
+        return sz;
+    }
+
+    public Reader getReader() {
+        return new CharBufferReader(chunks, count);
+    }
+
+    public void writeOut(Writer writer) throws IOException, IllegalArgumentException {
+        if (writer == null) {
+            throw new IllegalArgumentException("writer: may not be null.");
+        }
+        for (char[] chunk : this.chunks) {
+            if (chunk == this.currentChunk) {
+                break;
+            }
+            writer.write(chunk);
+        }
+        writer.write(currentChunk, 0, posInCurrentChunk);
+    }
+
+    @SuppressWarnings("SynchronizeOnNonFinalField")
+    private static class CharBufferReader extends Reader {
+
+        private final List<char[]> chunks;
+        private final int count;
+
+        private char[] currentChunk;
+        private int currentChunkIdx = -1;
+        private int posInCurrentChunk = 0;
+
+        private char[] markedChunk;
+        private int markedChunkIdx = -1;
+        private int markedPos = 0;
+
+        private CharBufferReader(List<char[]> chunks, int count) {
+            this.chunks = chunks;
+            this.count = count;
+            currentChunkIdx = chunks.size() == 0 ? -1 : 0;
+            currentChunk = chunks.size() == 0 ? null : chunks.get(0);
+        }
+
+        private void ensureOpen() throws IOException {
+            if (currentChunk == null) {
+                throw new IOException("Stream closed");
+            }
+        }
+
+        public int read() throws IOException {
+            synchronized (lock) {
+                ensureOpen();
+                if (posInCurrentChunk >= count) {
+                    return -1;
+                }
+                gotoNextReadableChunk();
+                return currentChunk[posInCurrentChunk++];
+            }
+        }
+
+        private int gotoNextReadableChunk() {
+            int availableChars = currentChunk.length - posInCurrentChunk;
+            if (availableChars > 0) {
+                return availableChars;
+            }
+            currentChunkIdx++;
+            currentChunk = chunks.get(currentChunkIdx);
+            posInCurrentChunk = 0;
+            return currentChunk.length;
+        }
+
+        @Override
+        public int read(char[] cbuf, int off, int len) throws IOException {
+            synchronized (lock) {
+                ensureOpen();
+                if ((off < 0) || (off > cbuf.length) || (len < 0) ||
+                    ((off + len) > cbuf.length) || ((off + len) < 0)) {
+                    throw new IndexOutOfBoundsException();
+                } else if (len == 0) {
+                    return 0;
+                }
+
+                if (posInCurrentChunk >= count) {
+                    return -1;
+                }
+                if (posInCurrentChunk + len > count) {
+                    len = count - posInCurrentChunk;
+                }
+                if (len <= 0) {
+                    return 0;
+                }
+                int remaining = len;
+                while (remaining > 0) {
+                    int charsToCopy = gotoNextReadableChunk();
+                    if (charsToCopy > remaining) {
+                        charsToCopy = remaining;
+                    }
+                    if (charsToCopy > 0) {
+                        System.arraycopy(currentChunk, posInCurrentChunk, cbuf, off, charsToCopy);
+                        off += charsToCopy;
+                        remaining -= charsToCopy;
+                        this.posInCurrentChunk += charsToCopy;
+                    }
+                }
+                return len;
+            }
+        }
+
+        public long skip(long n) throws IOException {
+            synchronized (lock) {
+                ensureOpen();
+                if (posInCurrentChunk + n > count) {
+                    n = count - posInCurrentChunk;
+                }
+                if (n < 0) {
+                    return 0;
+                }
+
+                int remaining = (int) n;
+                while (remaining > 0) {
+                    int charsToAdvance = gotoNextReadableChunk();
+                    if (charsToAdvance > remaining) {
+                        charsToAdvance = remaining;
+                    }
+                    if (charsToAdvance > 0) {
+                        remaining -= charsToAdvance;
+                        this.posInCurrentChunk += remaining;
+                    }
+                }
+                return n;
+            }
+        }
+
+        public boolean ready() throws IOException {
+            synchronized (lock) {
+                ensureOpen();
+                return (count - posInCurrentChunk) > 0;
+            }
+        }
+
+        public boolean markSupported() {
+            return true;
+        }
+
+        public void mark(int readAheadLimit) throws IOException {
+            synchronized (lock) {
+                ensureOpen();
+                markedPos = posInCurrentChunk;
+                markedChunk = currentChunk;
+                markedChunkIdx = currentChunkIdx;
+            }
+        }
+
+        public void reset() throws IOException {
+            synchronized (lock) {
+                ensureOpen();
+                posInCurrentChunk = markedPos;
+                currentChunk = markedChunk;
+                currentChunkIdx = markedChunkIdx;
+            }
+        }
+
+
+        @Override
+        public void close() throws IOException {
+            currentChunk = null;
+        }
+    }
+}
---
+++ src/test/java/org/apache/jasper/runtime/ChunkedBufferTest.java
@@ -0,0 +1,218 @@
+package org.apache.jasper.runtime;
+
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.StringWriter;
+import java.util.Arrays;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: vakopian
+ * Date: 3/17/14
+ * Time: 3:49 PM
+ */
+public class ChunkedBufferTest {
+
+    @Test
+    public void testBufferChar() {
+        ChunkedBuffer buf = new ChunkedBuffer(128);
+        for (int i = 0; i < 1047; ++i) {
+            buf.append('A');
+        }
+        assertEquals(1047, buf.length());
+        assertEquals(1458, buf.capacity()); //1458=128*1.5^6 > 1047 > 128*1.5^5=972
+    }
+
+    @Test
+    public void testBufferStringIntInt() {
+        StringBuilder sb = new StringBuilder(10240);
+        for (int i = 0; i < 10000; ++i) {
+            sb.append('A');
+        }
+
+        ChunkedBuffer buf = new ChunkedBuffer(128);
+        buf.append(sb.toString(), 100, 1047);
+        assertEquals(1047, buf.length());
+    }
+
+    @Test
+    public void testBufferCharArrayIntInt() {
+        char[] array = new char[10470];
+        for (int i = 0; i < array.length; ++i) {
+            array[i] = 'A';
+        }
+
+        ChunkedBuffer buf = new ChunkedBuffer(128);
+        buf.append(array, 765, 1047);
+        assertEquals(1047, buf.length());
+    }
+
+    @Test
+    public void testToArray() {
+        char[] array = new char[1047];
+        for (int i = 0; i < array.length; ++i) {
+            array[i] = 'A';
+        }
+
+        ChunkedBuffer buf = new ChunkedBuffer(128);
+        buf.append(array, 0, array.length);
+        assertEquals(1047, buf.length());
+        char[] result = buf.toArray();
+        assertTrue(Arrays.equals(array, result));
+    }
+
+    @Test
+    public void testToString() {
+        StringBuilder sb = new StringBuilder(10240);
+        for (int i = 0; i < 145; ++i) {
+            sb.append("ABCDEFGHIJKLMN");
+        }
+
+        ChunkedBuffer buf = new ChunkedBuffer(128);
+        buf.append(sb.toString(), 100, 1047);
+        assertEquals(1047, buf.length());
+        assertEquals(sb.substring(100, 1147), buf.toString());
+    }
+
+    @Test
+    public void testToArray2() {
+        ChunkedBuffer buf = new ChunkedBuffer(128);
+        char[] array = new char[1047];
+        char ch = 'A';
+        for (int i = 0; i < 1047; ++i) {
+            buf.append(ch);
+            array[i] = ch;
+        }
+
+        assertEquals(1047, buf.length());
+        char[] result = buf.toArray();
+        assertEquals(1047, result.length);
+        assertTrue(Arrays.equals(array, result));
+    }
+
+    @Test
+    public void testToString2() {
+        StringBuilder sb = new StringBuilder(10240);
+        ChunkedBuffer buf = new ChunkedBuffer(128);
+        String text = "ABCDEFGHIJKLMN";
+        for (int i = 0; i < 145; ++i) {
+            buf.append(text, 0, text.length());
+            sb.append(text);
+        }
+
+        assertEquals(145 * text.length(), buf.length());
+        assertEquals(sb.toString(), buf.toString());
+    }
+
+    @Test
+    public void testClear() {
+        StringBuilder sb = new StringBuilder(10240);
+        for (int i = 0; i < 145; ++i) {
+            sb.append("ABCDEFGHIJKLMN");
+        }
+
+        ChunkedBuffer buf = new ChunkedBuffer(128);
+        buf.append(sb.toString(), 100, 1047);
+
+        buf.clear();
+        assertEquals(0, buf.length());
+    }
+
+    @Test
+    public void testWriteOut() {
+        StringWriter writer = new StringWriter();
+
+        StringBuilder sb = new StringBuilder(10240);
+        for (int i = 0; i < 145; ++i) {
+            sb.append("ABCDEFGHIJKLMN");
+        }
+
+        ChunkedBuffer buf = new ChunkedBuffer(128);
+        buf.append(sb.toString(), 100, 1047);
+        assertEquals(1047, buf.length());
+        assertEquals(sb.substring(100, 1147), buf.toString());
+
+        try {
+            buf.writeOut(writer);
+        } catch (IOException e) {
+            fail(e.toString());
+        }
+        assertEquals(sb.substring(100, 1147), writer.toString());
+
+    }
+
+    @Test
+    public void testSetLength() {
+        ChunkedBuffer active = new ChunkedBuffer();
+        active.append("first one");
+        String a = active.toString();
+        active.setLength(0);
+        active.append("second");
+        String b = active.toString();
+        active.setLength(3);
+        assertEquals("ChunkedBuffer.setLength() overwrote string", "first one", a);
+        assertEquals("second", b);
+        assertEquals(3, active.length());
+        assertTrue(active.capacity() > 3);
+        active.append("tion");
+        assertEquals("section", active.toString());
+    }
+
+    @Test
+    public void testGetChars() {
+        final int dstLen = 10;
+        char[] dst = new char[dstLen];
+        for (int i = 0; i < dstLen; i++) {
+            dst[i] = 'Z';
+        }
+        final int srcBegin = 3;
+        final int srcEnd = 9;
+        final int dstBegin = 2;
+        ChunkedBuffer cb = new ChunkedBuffer(2);
+        cb.append("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+        cb.getChars(srcBegin, srcEnd, dst, dstBegin);
+        for (int i = 0; i < dstLen; ++i) {
+            if (i >= dstBegin && i < dstBegin + (srcEnd - srcBegin)) {
+                assertEquals('A' + srcBegin + (i- dstBegin), dst[i]);
+            } else {
+                assertEquals('Z', dst[i]);
+            }
+        }
+    }
+
+    @Test
+    public void testGetCharsSrcEndLarger() {
+        try {
+            new ChunkedBuffer("abc").getChars(1, 0, new char[10], 0);
+            fail("StringBuffer.getChars() must throw an exception if srcBegin > srcEnd");
+        } catch (StringIndexOutOfBoundsException sioobe) {
+        }
+    }
+
+    @Test
+    public void testAppendLargeBuffer() {
+
+        ChunkedBuffer cb = new ChunkedBuffer("");
+        try {
+            cb.append(new char[5], 1, Integer.MAX_VALUE);
+            fail();
+        } catch (StringIndexOutOfBoundsException sobe) {
+            // Test passed
+        } catch (OutOfMemoryError oome) {
+            fail("Wrong exception thrown.");
+        }
+
+        ChunkedBuffer sb1 = new ChunkedBuffer("Some test StringBuffer");
+        try {
+            sb1.append(new char[25], 5, Integer.MAX_VALUE);
+            fail();
+        } catch (StringIndexOutOfBoundsException sobe) {
+            // Test passed
+        } catch (ArrayIndexOutOfBoundsException aioe) {
+            fail("Wrong exception thrown.");
+        }
+    }
+}
